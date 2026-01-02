889cd49b10bae4d2028be29960149ed7a0dbd81a
Aman Dwivedi <amandwivedi262@gmail.com>
2026-01-02 18:47:24 +0530

    first commit

---
+++ youtubeFrontend/src/utils/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-2xl  text-gray-900">
+        YouTube Clone Frontend
+      </h1>
+
+    </>
+  )
+}
+
+export default App
