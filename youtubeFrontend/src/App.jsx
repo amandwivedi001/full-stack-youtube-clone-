ad91ac4805f04380edd0a9767f795bd32b122c36
Aman Dwivedi <amandwivedi262@gmail.com>
2026-01-28 01:10:55 +0530

    frontend

--- youtubeFrontend/src/App.jsx
+++ youtubeFrontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 class="text-2xl  text-gray-900">
+      <h1 class="bg-amber-700 text-2xl  text-gray-900">
         YouTube Clone Frontend
       </h1>
 
