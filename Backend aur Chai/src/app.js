146af597a67670d44a4f02708dac164dcbbdbeb0
Aman Dwivedi <amandwivedi262@gmail.com>
2026-02-25 01:17:18 +0530

    added route

--- Backend aur Chai/src/app.js
+++ Backend aur Chai/src/app.js
@@ -27,6 +27,7 @@ import dashboardRouter from "./routes/dashboard.route.js"
 
 
 //routes declaration
+app.get("/", (req, res) => {res.send("Backend is running 🚀");});
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
