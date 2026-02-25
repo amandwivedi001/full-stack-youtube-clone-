d44e25713d1c70a790aecf19feb76daf88f6f0b3
Aman Dwivedi <amandwivedi262@gmail.com>
2026-02-25 19:39:06 +0530

    add some commands to tell express request coming from https behind proxy

--- Backend aur Chai/src/app.js
+++ Backend aur Chai/src/app.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -27,7 +29,7 @@ import dashboardRouter from "./routes/dashboard.route.js"
 
 
 //routes declaration
-app.get("/", (req, res) => {res.send("Backend is running 🚀");});
+app.get("/", (req, res) => { res.send("Backend is running 🚀"); });
 app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
@@ -38,4 +40,4 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 
-export {app}
+export { app }
